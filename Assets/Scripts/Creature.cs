3b07e07e1630d680fcd80101011f49fecccf8e8f
Scorr <scorrdev@gmail.com>
2016-06-05 01:28:45 +0200

    Basic creature class.

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Creature
+{
+    public enum CreatureType
+    {
+        Slime,
+        Dragon,
+        Griffon,
+        Nymph,
+        Equine,
+        Demon,
+        Beast,
+        Golem,
+        Bug,
+        Avian
+    }
+
+    public CreatureType Type { get; private set; }
+    public int Health { get; private set; }
+    public int Libido { get; private set; }
+}

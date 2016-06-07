0d6964b6de5282b7995874c283a0756ec3d5fc50
David Birchall <kal_agrim@hotmail.co.uk>
2016-06-06 19:12:06 -0700

    UI Screens Updated
    * Implemented Canvas Switching System for Game Screens
    * Implemented General Menu Screen
    * Implemented Store Screen
    * Implemented Management Screen
    * Implemented Navigation Buttons

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasManager : MonoBehaviour {
+
+    [SerializeField] private GameObject generalCanvas;
+    [SerializeField] private GameObject storeCanvas;
+    [SerializeField] private GameObject managementCanvas;
+    
+    void Start()
+    {
+        if (generalCanvas == null)
+        {
+            generalCanvas = GameObject.Find("GeneralMenuCanvas") as GameObject;
+        }
+        if (storeCanvas == null)
+        {
+            storeCanvas = GameObject.Find("StoreCanvas") as GameObject;
+        }
+        if (managementCanvas == null)
+        {
+            managementCanvas = GameObject.Find("ManagementCanvas") as GameObject;
+        }
+
+        ShowGeneralMenuScreen();
+    }
+
+    public void ShowGeneralMenuScreen()
+    {
+        generalCanvas.SetActive(true);
+        storeCanvas.SetActive(false);
+        managementCanvas.SetActive(false);
+    }
+
+    public void ShowStoreScreen()
+    {
+        generalCanvas.SetActive(false);
+        storeCanvas.SetActive(true);
+        managementCanvas.SetActive(false);
+    }
+
+    public void ShowManagementScreen()
+    {
+        generalCanvas.SetActive(false);
+        storeCanvas.SetActive(false);
+        managementCanvas.SetActive(true);
+    }
+}

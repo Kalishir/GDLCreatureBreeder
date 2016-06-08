d7ecbac674261035a08ee59d50bfc1a784c34527
Thaddeus <oni_reborn@hotmail.com>
2016-06-08 13:57:22 +1000

    * Added basic sound effects and music * Cleaned up folders and GameObject hierarchy even more

---
+++ Assets/Menu/Scripts/GeneralUIHelper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneralUIHelper : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+
+    /// <summary>
+    /// The start method
+    /// </summary>
+    private void Start()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// Quick and simple SFX Player for UI elements
+    /// </summary>
+    /// <param name="clip">
+    /// The clip.
+    /// </param>
+    public void PlaySound(AudioClip clip)
+    {
+        this.audioSource.clip = clip;
+        this.audioSource.Play();
+    }
+
+
+
+}

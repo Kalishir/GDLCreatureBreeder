dda3efb0305037343621d36c637941d65143bb95
scnoobi <scnoobers@gmail.com>
2016-06-09 21:20:58 +0200

    Added uiManager so we can highlight selected items in the hierarchy *implemented it in the scene too (+1 squashed commits)
    Squashed commits:
    
    [bc94db2] updated scrolling values

---
+++ Assets/UIManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    private List<Image> images = new List<Image>(10);
+    private Color originalColor;
+    public Color selectedItemColor = Color.blue;
+
+    public void ItemSelected(Image theImage)
+    {
+        //First check if an image like the one we got. actually exists
+        bool exists = false;
+        for (int i = 0; i < images.Count; i++)
+        {
+            if (images[i] == theImage)
+            {
+                exists = true;
+            }
+        }
+        if (exists == false)
+        {
+            images.Add(theImage);
+            originalColor = theImage.color;
+        }
+
+        SetTheColor(theImage);
+    }
+
+    private void SetTheColor(Image theImage)
+    {
+        for (int i = 0; i < images.Count; i++)
+        {
+            if (images[i] == theImage)
+                theImage.color = selectedItemColor;
+            else
+            {
+                images[i].color = originalColor;
+            }
+        }
+    }
+}

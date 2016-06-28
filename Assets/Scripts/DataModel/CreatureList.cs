8098ad5cf7aab76e41ea01202a579a8deba4ddb4
Kalishir <kal_agrim@hotmail.co.uk>
2016-06-28 10:35:51 -0700

    Updated CreatureList.cs
    Added XML comments.

--- Assets/Scripts/DataModel/CreatureList.cs
+++ Assets/Scripts/DataModel/CreatureList.cs
@@ -8,7 +8,6 @@ public class CreatureList
     [SerializeField] private List<Creature> creatures;
     // TODO: Refactor so that creatures cannot be added directly to the list;
 
-
     /// <summary>
     /// Gets the creature list.
     /// </summary>
@@ -18,24 +17,37 @@ public class CreatureList
         private set { creatures = value; }
     }
 
+    /// <summary>
+    /// Creates a new CreatureList object. Initializes the internal list.
+    /// </summary>
     public CreatureList()
     {
         creatures = new List<Creature>();
     }
 
+    /// <summary>
+    /// Generates a new Creature object from a CreatureData object and adds it to the internal list.
+    /// </summary>
     public void AddCreature(CreatureData creatureData)
     {
         Creature newCreature = new Creature(creatureData);
         creatures.Add(newCreature);
     }
 
+    /// <summary>
+    /// Adds an existing Creature object to the internal list.
+    /// </summary>
     public void AddCreature(Creature creature)
     {
         creatures.Add(creature);
     }
 
+    /// <summary>
+    /// Removes an existing Creature object from the internal list.
+    /// </summary>
     public void RemoveCreature(Creature creature)
     {
+        //TODO: RemoveCreature by GUID.
         creatures.Remove(creature);
     }
 

7c42b7224d21dc8cb3b1f762655f125d399beaa1
David Birchall <kal_agrim@hotmail.co.uk>
2016-07-16 20:42:15 -0700

    Created a mockup of a new UI version. Nothing is correctly functional. Some script support for the menu buttons was added.

---
+++ Assets/MenuButtonSelectionHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuButtonSelectionHandler : MonoBehaviour {
+
+    bool selected = false;
+    [SerializeField] private float positionModifier;
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private Color selectedColour;
+    [SerializeField] private Color backgroundColour;
+
+    void Start()
+    {
+        if (backgroundImage == null)
+            backgroundImage = transform.Find("Background").GetComponent<Image>();
+
+        if(backgroundImage != null)
+            backgroundImage.color = backgroundColour;
+    }
+
+
+    public void OnSelected()
+    {
+        float newX = transform.position.x;
+        selected = !selected;
+
+        if (selected)
+        {
+            newX += positionModifier;
+            if (backgroundImage != null)
+                backgroundImage.color = selectedColour;
+        }
+        else
+        {
+            newX -= positionModifier;
+            if (backgroundImage != null)
+                backgroundImage.color = backgroundColour;
+        }
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+}
